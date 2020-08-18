cd278e4cf23794897bc1f705ae5465b175415c1c
dustins <dustin_seah@sfu.ca>
2020-08-17 18:47:11 -0700

    Finalized

--- project/compress.c
+++ project/compress.c
@@ -7,7 +7,7 @@ unsigned long file_size(char *filename)
 {
     FILE *pFile = fopen(filename, "rb");
     fseek (pFile, 0, SEEK_END);
-   unsigned long size = ftell(pFile);
+    unsigned long size = ftell(pFile);
     fclose (pFile);
     return size;
 }
@@ -16,6 +16,7 @@ int compressImage(char* input_file_name, char* output_file_name)
 {   
     FILE *infile = fopen(input_file_name, "rb");
     gzFile outfile = gzopen(output_file_name, "wb");
+    
     if (!infile || !outfile)
     {
         return -1;
@@ -27,15 +28,17 @@ int compressImage(char* input_file_name, char* output_file_name)
     int num_read = 0;
     unsigned long total_read = 0;
     unsigned long total_wrote = 0;
+    
     while ((num_read = fread(inbuffer, 1, sizeof(inbuffer), infile)) > 0)
     {
        total_read += num_read;
        gzwrite(outfile, inbuffer, num_read);
     }
+    
     fclose(infile);
     gzclose(outfile);
 
-    printf("Read %ld bytes, Wrote %ld bytes, Compression factor %4.2f%%\n",total_read, file_size(output_file_name), (1.0-file_size(output_file_name)*1.0/total_read)*100.0);
+    printf("Read %ld bytes, Wrote %ld bytes, Compression factor: %4.2f%%\n", total_read, file_size(output_file_name), (1.0-file_size(output_file_name)*1.0/total_read)*100.0);
 
 }
 
